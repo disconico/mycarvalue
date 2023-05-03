a75fb0b6d7dbff6765b4a2504dd5b4f6b38186b9
Nicolas Guerin <disconico@outlook.com>
2023-05-03 10:39:29 +0200

    Add query builder with TypeORM

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,6 +3,7 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './report.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { User } from '../users/user.entity';
 
 @Injectable()
@@ -24,5 +25,28 @@ export class ReportsService {
     } catch (error) {
       throw new NotFoundException('Report not found');
     }
+  }
+
+  async createEstimate({
+    make,
+    model,
+    year,
+    lat,
+    lng,
+    mileage,
+  }: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng -  :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat -  :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
   }
 }
